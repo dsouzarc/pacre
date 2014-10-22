0ecd4266d20641cb2b406e9434969a7e1680f42e
Ryan D'souza <dsouzarc@gmail.com>
2014-10-22 19:08:40 -0400

    Added SendGrid.jar file, is integrated

--- app/src/main/java/com/ryan/recap/CreateAccountActivity.java
+++ app/src/main/java/com/ryan/recap/CreateAccountActivity.java
@@ -18,7 +18,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-
+import com.github.sendgrid.SendGrid;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
