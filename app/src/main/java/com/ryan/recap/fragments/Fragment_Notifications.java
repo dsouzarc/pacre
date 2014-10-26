d5e648290856a9a8758d76852cb982e960a8ebb6
Ryan D'souza <dsouzarc@gmail.com>
2014-10-26 18:18:23 -0400

    Randomly adds 50 notifications. Working version

--- app/src/main/java/com/ryan/recap/fragments/Fragment_Notifications.java
+++ app/src/main/java/com/ryan/recap/fragments/Fragment_Notifications.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import com.ryan.recap.objects.Person;
+import com.ryan.recap.objects.Notification;
+import android.widget.LinearLayout;
+import com.ryan.recap.views.NotificationView;
 import android.view.ViewGroup;
 
 import com.ryan.recap.R;
@@ -17,7 +21,20 @@ public class Fragment_Notifications extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
+        this.theC = getActivity().getApplicationContext();
         final View theView = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        final LinearLayout theLayout = (LinearLayout) theView.findViewById(R.id.theLayout);
+
+        final Person me = new Person("Ryan D'souza", "dsouzarc@gmail.com",
+                getResources().getDrawable(R.drawable.blankprofile));
+
+        for(int i = 0; i < 50; i++) {
+            final Notification notif = new Notification(me, me.getName() + " liked your recap " + i,
+                    String.valueOf(i), null);
+            theLayout.addView(new NotificationView(theC, notif));
+        }
+
         return theView;
     }
 
