be11e3340ff4e5cf1a70ced4d663ce2836308bc1
Ryan D'souza <dsouzarc@gmail.com>
2014-10-26 17:50:22 -0400

    Implemented class

--- app/src/main/java/com/ryan/recap/objects/Notification.java
+++ app/src/main/java/com/ryan/recap/objects/Notification.java
@@ -1,7 +1,32 @@
 package com.ryan.recap.objects;
 
-/**
- * Created by Ryan on 10/26/14.
- */
+import android.graphics.drawable.Drawable;
+
 public class Notification {
+    private final Person otherPerson;
+    private final String message, time;
+    private Drawable otherDrawable;
+
+    public Notification(Person otherPerson, String message, String time, Drawable otherDrawable) {
+        this.otherPerson = otherPerson;
+        this.message = message;
+        this.time = time;
+        this.otherDrawable = otherDrawable;
+    }
+
+    public Person getOtherPerson() {
+        return otherPerson;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public Drawable getOtherDrawable() {
+        return otherDrawable;
+    }
 }
