8a352df372bd0314896a98fc8696a0932d29f710
Ryan D'souza <dsouzarc@gmail.com>
2014-10-26 14:34:08 -0400

    Class to act as home fragment

---
+++ app/src/main/java/com/ryan/recap/Home_Fragment.java
@@ -0,0 +1,11 @@
+package com.ryan.recap;
+
+import android.support.v4.app.Fragment;
+import android.view.ViewGroup;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+import android.content.Context;
+public class Home_Fragment {
+    
+}
