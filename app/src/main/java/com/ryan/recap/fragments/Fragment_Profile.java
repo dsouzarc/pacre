dc8ac9d3981a4a373be8e6b1cd5faec6d8af787a
Ryan D'souza <dsouzarc@gmail.com>
2014-11-05 05:58:08 -0500

    Code to update prof. pic

--- app/src/main/java/com/ryan/recap/fragments/Fragment_Profile.java
+++ app/src/main/java/com/ryan/recap/fragments/Fragment_Profile.java
@@ -7,7 +7,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Toast;
 import com.ryan.recap.R;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.pkmmte.view.CircularImageView;
+import java.io.FileInputStream;
+import com.ryan.recap.Constants;
+import android.os.AsyncTask;
 
 public class Fragment_Profile extends Fragment {
     private static Context theC;
@@ -19,7 +43,45 @@ public class Fragment_Profile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
         final View theView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        new ShowPicture((ImageView) theView.findViewById(R.id.profilePicture)).execute(Constants.PROFILE_PICTURE);
+
+
         return theView;
     }
+
+    private class ShowPicture extends AsyncTask<String, Void, Bitmap> {
+        private final ImageView view;
+
+        public ShowPicture(final ImageView view) {
+            this.view = view;
+        }
+
+        @Override
+        public Bitmap doInBackground(final String... fileNames) {
+            try {
+                final FileInputStream fis = theC.openFileInput(Constants.PROFILE_PICTURE);
+                return BitmapFactory.decodeStream(fis);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        public void onPostExecute(final Bitmap bm) {
+            if(bm == null) {
+                makeToast("Error retrieving profile photo");
+                return;
+            }
+
+            view.setImageBitmap(bm);
+        }
+    }
+
+    public void makeToast(final String m) {
+        Toast.makeText(theC, m, Toast.LENGTH_LONG).show();
+    }
+
 
 }
